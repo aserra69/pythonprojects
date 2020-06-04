296d8df383ab4623719e69368f3af1b2d07d8632
Angelo Serra <aserra69@yahoo.com>
2020-06-04 17:07:43 -0400

    Update fibonacci_numbers.py
    Updated to work under Python3 - mainly changes to print commands.

--- fibonacci_numbers.py
+++ fibonacci_numbers.py
@@ -1,6 +1,8 @@
 # program to determine Fibonacci numbers - an exploration of Python
 # using the Liber Abaci sequence that starts with a 1
-#setup
+# written september 2013
+# updated for python 3 June 2020
+# setup
 overall_number = 0
 start_string = "Calculation of Liber Abaci Fibonacci Series"
 inquiry_string = "How many numbers would you like in the series? "
@@ -8,13 +10,17 @@ sequence_string = "Fibonacci Series Number"
 fibonacci_one = 0
 fibonacci_two = 1
 
-#main
-print "%s" % (start_string)
-series_number = input(inquiry_string)
+#main program
+#prompt the user
+print(start_string)
+#Take size of series and assign to veriable as an integer
+series_number = int(input(inquiry_string))
+#Loop through the series size requested
 for series in range(1,series_number+1):
   #Calculate Fibonacci Number
   fibonacci = fibonacci_one + fibonacci_two
-  print "%s %d : %d" % (sequence_string, series, fibonacci)
+  print(sequence_string, series, fibonacci)
+  #Move number to prepare for next cycle
   fibonacci_two = fibonacci_one
   fibonacci_one = fibonacci
 	
